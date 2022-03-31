d054641c2a69c206ded6802b6d3f15174a87654a
moabjr <moabjr2@gmail.com>
2022-03-31 15:21:26 -0300

    Ajustando o admin do app estoque

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from .models import Estoque, EstoqueItens
+
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display=(
+        '__str__',
+        'funcionario',
+        'nf',
+        'movimento',
+    )
+    search_fields=('nf',)
+    list_filter=('funcionario',)
+    date_hierarchy = 'created'
