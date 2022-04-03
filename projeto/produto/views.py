b09e5989030ca8e0673bfaaf6ade6cf5a5bb071f
moabjr <moabjr2@gmail.com>
2022-04-03 20:52:29 -0300

    Atualizações do sistema

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def produto_list(request):
+    template_list = 'produto_list.html'
+    objects = Produto.objects.all()
+    context = {'object_list': objects}
+    return render(request, template_list, context)
