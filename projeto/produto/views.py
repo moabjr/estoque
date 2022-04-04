acbac755a46892af94ba638823065337801d2983
moabjr <moabjr2@gmail.com>
2022-04-03 21:48:32 -0300

    Atualizações do sistema

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import Produto
 
 def produto_list(request):
-    template_list = 'produto_list.html'
+    template_name = 'produto_list.html'
     objects = Produto.objects.all()
     context = {'object_list': objects}
-    return render(request, template_list, context)
+    return render(request, template_name, context)
+
+def produto_detail(request, pk):
+    template_name = 'produto_detail.html'
+    obj = Produto.objects.get(pk=pk)
+    context = {'object': obj}
+    return render(request, template_name, context)
